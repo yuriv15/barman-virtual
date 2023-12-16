548277b189d505b3d014b8c8e553030103dcd027
Yuri Gonçalves <yuri.goncalves@dsgtechnology.com.br>
2023-12-16 18:11:37 -0300

    Adicionada fonte Roboto como padrão do projeto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: [
         ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }],
+        '@nuxt/image',
     ],
     imports: {
         dirs: ['stores'],
@@ -10,4 +11,5 @@ export default defineNuxtConfig({
     alias: {
         pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
     },
+    css: ['~/assets/scss/main.scss'],
 });
