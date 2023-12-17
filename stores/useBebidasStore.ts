afd1ac4bb3bcb17d8a28fc4caa474c1cec086d5d
Yuri Gonçalves <yuri.goncalves@dsgtechnology.com.br>
2023-12-17 13:54:04 -0300

    update: incluído filtragens na store de bebidas.

--- stores/useBebidasStore.ts
+++ stores/useBebidasStore.ts
@@ -1,4 +1,5 @@
 import type CatalogoBebidas from '~/interfaces/bebidas';
+import type { FiltroCategoria } from '~/interfaces/filtros';
 
 export const useBebidasStore = defineStore('filtros', () => {
     const carregandoBebidas = ref<boolean>(true);
@@ -10,41 +11,53 @@ export const useBebidasStore = defineStore('filtros', () => {
     async function carregarCatalogoBebidas() {
         carregandoBebidas.value = true;
 
-        if (!catalogoBebidas.value.drinks.length) {
-            const { pending: ordinaryPendente, data: catalogoOrdinary } =
-                await useFetch<CatalogoBebidas>(
-                    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink'
-                );
-
-            const { pending: cocktailPendente, data: catalogoCocktail } =
-                await useFetch<CatalogoBebidas>(
-                    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
-                );
-
-            if (!(ordinaryPendente.value && cocktailPendente.value)) {
-                if (catalogoOrdinary.value) {
-                    catalogoBebidas.value.drinks.push(
-                        ...catalogoOrdinary.value.drinks
+        await nextTick(async () => {
+            if (!catalogoBebidas.value.drinks.length) {
+                const { pending: ordinaryPendente, data: catalogoOrdinary } =
+                    await useFetch<CatalogoBebidas>(
+                        'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink'
                     );
-                }
-                if (catalogoCocktail.value) {
-                    catalogoBebidas.value.drinks.push(
-                        ...catalogoCocktail.value.drinks
-                    );
-                }
 
-                catalogoBebidas.value.drinks =
-                    catalogoBebidas.value.drinks.sort((a, b) => {
-                        if (a.strDrink < b.strDrink) {
-                            return -1;
-                        }
-                        if (a.strDrink > b.strDrink) {
-                            return 1;
-                        }
-                        return 0;
-                    });
+                const { pending: cocktailPendente, data: catalogoCocktail } =
+                    await useFetch<CatalogoBebidas>(
+                        'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+                    );
+
+                if (!(ordinaryPendente.value && cocktailPendente.value)) {
+                    if (catalogoOrdinary.value) {
+                        catalogoOrdinary.value.drinks.forEach((drink) => {
+                            drink.strCategory = 'Ordinary Drink';
+                        });
+
+                        catalogoBebidas.value.drinks.push(
+                            ...catalogoOrdinary.value.drinks
+                        );
+                    }
+
+                    if (catalogoCocktail.value) {
+                        catalogoCocktail.value.drinks.forEach((drink) => {
+                            drink.strCategory = 'Cocktail';
+                        });
+
+                        catalogoBebidas.value.drinks.push(
+                            ...catalogoCocktail.value.drinks
+                        );
+                    }
+
+                    catalogoBebidas.value.drinks =
+                        catalogoBebidas.value.drinks.sort((a, b) => {
+                            if (a.strDrink < b.strDrink) {
+                                return -1;
+                            }
+                            if (a.strDrink > b.strDrink) {
+                                return 1;
+                            }
+                            return 0;
+                        });
+                }
+                carregarBebidasFavoritas();
             }
-        }
+        });
 
         carregandoBebidas.value = false;
     }
@@ -55,14 +68,67 @@ export const useBebidasStore = defineStore('filtros', () => {
         );
     });
 
+    watch(
+        bebidasFavoritas,
+        (bebidas) => {
+            localStorage.setItem('bebidasFavoritas', JSON.stringify(bebidas));
+        },
+        { deep: true }
+    );
+
+    function carregarBebidasFavoritas() {
+        const bebidasFavoritas = localStorage.getItem('bebidasFavoritas');
+
+        if (bebidasFavoritas) {
+            const bebidasFavoritasJSON = JSON.parse(bebidasFavoritas);
+
+            catalogoBebidas.value.drinks.forEach((bebida) => {
+                bebida.isFavorita = bebidasFavoritasJSON.some(
+                    (bebidaFavorita: any) =>
+                        bebidaFavorita.idDrink === bebida.idDrink
+                );
+            });
+        }
+    }
+
     const filtroBusca = ref<string>('');
-    const filtroCategoria = ref<string>('');
+    const filtroCategoria = ref<FiltroCategoria>('');
+
+    const catalogoBebidasFiltradas = computed(() => {
+        const catalogoFiltrado: CatalogoBebidas = {
+            drinks: [],
+        };
+
+        if (filtroCategoria.value === 'Favoritos') {
+            catalogoFiltrado.drinks.push(
+                ...catalogoBebidas.value.drinks.filter(
+                    (bebida) => bebida.isFavorita
+                )
+            );
+        } else if (filtroCategoria.value) {
+            catalogoFiltrado.drinks.push(
+                ...catalogoBebidas.value.drinks.filter(
+                    (bebida) => bebida.strCategory === filtroCategoria.value
+                )
+            );
+        } else {
+            catalogoFiltrado.drinks.push(...catalogoBebidas.value.drinks);
+        }
+
+        if (filtroBusca.value) {
+            catalogoFiltrado.drinks = catalogoFiltrado.drinks.filter((bebida) =>
+                bebida.strDrink.toLowerCase().includes(filtroBusca.value)
+            );
+        }
+
+        return catalogoFiltrado;
+    });
 
     function setFiltroBusca(value: string) {
         filtroBusca.value = value;
     }
 
-    function setFiltroCategoria(value: string) {
+    function setFiltroCategoria(value: FiltroCategoria) {
         filtroCategoria.value = value;
     }
 
@@ -82,7 +148,7 @@ export const useBebidasStore = defineStore('filtros', () => {
         toggleFavorito,
         carregarCatalogoBebidas,
         carregandoBebidas,
-        catalogoBebidas,
+        catalogoBebidas: catalogoBebidasFiltradas,
         bebidasFavoritas,
     };
 });
