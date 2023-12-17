435186ec6f8104e3d378dc82689c7ac789be6b90
Yuri Gonçalves <yuri.goncalves@dsgtechnology.com.br>
2023-12-17 19:22:50 -0300

    update: melhorado componente de busca

--- components/Filtro/Busca.vue
+++ components/Filtro/Busca.vue
@@ -1,15 +1,22 @@
 <template>
     <div class="filtro-container">
-        <input
-            type="text"
-            placeholder="Pesquisar por nome..."
-            aria-label="Pesquisar por nome"
-            aria-describedby="botao-pesquisar"
-            v-model="filtro"
-            @keyup.enter="setFiltroBusca(filtro)"
-        />
+        <div class="input-container">
+            <input
+                type="text"
+                placeholder="Pesquisar por nome..."
+                aria-label="Pesquisar por nome"
+                aria-describedby="botao-pesquisar"
+                v-model="filtro"
+                @keyup.enter="setFiltroBusca(filtro)"
+            />
+            <IconeFechar
+                v-if="filtro"
+                class="icone-fechar"
+                @click="limparFiltro"
+                :width="30"
+            />
+        </div>
         <button
-            class="btn btn-outline-secondary"
             type="button"
             id="botao-pesquisar"
             @click="setFiltroBusca(filtro)"
@@ -24,6 +31,11 @@ const filtro = ref('');
 
 const bebidasStore = useBebidasStore();
 const { setFiltroBusca } = bebidasStore;
+
+function limparFiltro() {
+    setFiltroBusca('');
+    filtro.value = '';
+}
 </script>
 
 <style scoped lang="scss">
@@ -33,14 +45,27 @@ const { setFiltroBusca } = bebidasStore;
     gap: 8px;
     height: 50px;
 
-    input {
+    .input-container {
+        position: relative;
         flex-grow: 1;
         height: 100%;
-        font-size: 1.1rem;
-        border-radius: 5px;
-        border: 1px solid #cecece;
-        background-color: #2c2c2c;
-        padding: 0 12px;
+        display: flex;
+        align-items: center;
+
+        input {
+            flex-grow: 1;
+            height: 100%;
+            font-size: 1.1rem;
+            border-radius: 5px;
+            border: 1px solid #cecece;
+            background-color: #2c2c2c;
+            padding: 0 45px 0 12px;
+        }
+
+        .icone-fechar {
+            position: absolute;
+            right: 10px;
+        }
     }
 
     button {
